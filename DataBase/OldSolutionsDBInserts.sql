0fd6ecde3583edc8a75ba562f1b828b8ffed107b
Adán <adanjogohe@gmail.com>
2018-12-17 17:27:03 +0000

    Añadiendo el nuevo readme e inserts

--- DataBase/OldSolutionsDBInserts.sql
+++
@@ -1,8 +0,0 @@
--- PLANTILLAS
-/* Insert en Cliente con el ID Autogenerado
-insert into CLIENTE(telefono_contacto,nombre) values ("telefono","nombre");
-*/
-/* Insert en Operador con el ID Autogenerado
-insert into OPERADOR(dni,nombre,apellidos) values ("dni","nombre","apellidos");
-*/
---
---
+++ DataBase/OldSolutionsInserts.sql
@@ -0,0 +1,58 @@
+-- PLANTILLAS
+/* Insert en Cliente con el ID Autogenerado
+insert into CLIENTE(telefono_contacto,nombre,password) values ("telefono","nombre","password");
+*/
+/* Insert en Operador con el ID Autogenerado
+insert into OPERADOR(dni,nombre,apellidos,password) values ("dni","nombre","apellidos","password");
+*/
+/*Insert en Producto con el ID Autogenerado
+insert into OPERADOR(nombre,descripcion,precio_publico,url_imagen) values ("nombre","descripcion",0,"url")
+-- Inserts
+/* Inserts de clientes
+insert into CLIENTE(telefono_contacto,nombre,password) values ("691417400","Lupsem","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("691414001","Darko","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("691417002","Ninja","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("691474003","Dirl","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("691174004","Surl","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("694174005","Vrto","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("614174006","Grast","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("914174007","Manos","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("691414008","Arco","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("691474009","Juana","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("691174011","Blyat","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("694174122","Apso","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("614174013","Jusic","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("914174014","Manic","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("691417415","Dares","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("691414016","Bals","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("691474017","Blase","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("691174018","Praco","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("694174019","Dorki","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("141740020","Tfue","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("691417021","ASdr","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("691417422","Anon","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("691417023","Cruck","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("691414024","Porsi","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("691474025","Flucki","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("691174026","Flash","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("694174027","Tarco","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("614174028","Perse","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("691417029","Plato","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("691414030","Willy","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("691474031","Guille","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("691174032","Kakso","qwerty");
+insert into CLIENTE(telefono_contacto,nombre,password) values ("694174033","Derksu","qwerty");
+*/
+/*Inserts de operadores (no hay control de DNI por parte de la base de datos, los operadores pueden no tener contraseña.) Seguridad de las contraseñas https://cursoprogramador.wordpress.com/2014/01/16/tabla-para-almacenar-usuario-y-contrasena-de-aplicacion/
+insert into OPERADOR(dni,nombre,apellidos,password) values ("45346569E","Adan","Leonhardt",SHA1('adminadmin'));
+insert into OPERADOR(dni,nombre,apellidos,password) values ("45715421X","Croz","Braton",SHA1('admin'));
+insert into OPERADOR(dni,nombre,apellidos,password) values ("36541154D","Elon","Musto",SHA1('root'));
+insert into OPERADOR(dni,nombre,apellidos,password) values ("14557897X","Mary","Blosa",SHA1('qwerty'));
+insert into OPERADOR(dni,nombre,apellidos,password) values ("12151516X","Marc","Plisto",SHA1('qwerty123'));
+*/
+/* Inserts de Productos
+insert into OPERADOR(nombre,descripcion,precio_publico,url_imagen) values ("Tarjeta grafica","Tarjeta de video externa",120,"https://i.imgur.com/krFP9EG.jpg");
+insert into OPERADOR(nombre,descripcion,precio_publico,url_imagen) values ("CPU","Microprocesador Intel de gran capacidad",160,"https://i.imgur.com/Ak2MhPQ.jpg");
+insert into OPERADOR(nombre,descripcion,precio_publico,url_imagen) values ("RAM","Memoria de alta velocidad",60,"https://i.imgur.com/gqX0DXq.jpg");
+insert into OPERADOR(nombre,descripcion,precio_publico,url_imagen) values ("Placa base","Placa madre donde se instalan todos los componentes",90,"https://i.imgur.com/zXUQWSM.jpg");
+*/
