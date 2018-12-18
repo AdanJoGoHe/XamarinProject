6067c72d36fc88944c97f5ced633e6f5cb6b0fe0
Adán <adanjogohe@gmail.com>
2018-12-18 13:00:33 +0000

    Creado Unit Test para la comprobación Hash

---
+++ oldSolutions/Hash/HashTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using oldSolutions;
+
+namespace Hash
+{
+    [TestClass]
+    public class HashTest
+    {
+        [TestMethod]
+        public void Test_Hash()
+        {
+            //Arrange
+            string expected = "dd94709528bb1c83d08f3088d4043f4742891f4f";
+            string toTest = "adminadmin";
+
+            //Act
+            Core core = new Core();
+            string actual = core.Hash(toTest);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
